32bb7f681261c9da7b9ab9dbc54ff9934e915b8d
Sik <contact@sikelio.wtf>
2024-03-23 23:57:24 +0100

    Added frontend UX for websocket control

--- src/controllers/app_controller.ts
+++ src/controllers/app_controller.ts
@@ -6,12 +6,15 @@ import storeManager from '../libs/StoreManager';
 import type { ISocketConfig } from '../interfaces/ISocket';
 
 export default class app_controller extends Controller {
-    public static targets: string[] = ['ip', 'devices', 'port', 'token'];
+    public static targets: string[] = ['ip', 'devices', 'port', 'token', 'startbtn', 'stopbtn', 'servivestatus'];
 
     declare readonly ipTarget: HTMLSpanElement;
     declare readonly devicesTarget: HTMLUListElement;
     declare readonly portTarget: HTMLSpanElement;
     declare readonly tokenTarget: HTMLSpanElement;
+    declare readonly startbtnTarget: HTMLButtonElement;
+    declare readonly stopbtnTarget: HTMLButtonElement;
+    declare readonly servivestatusTarget: HTMLSpanElement;
 
     public async connect() {
         const ip: string = await invoke('get_ip');
@@ -30,6 +33,38 @@ export default class app_controller extends Controller {
         } else {
             this.portTarget.textContent = (socketConfig as ISocketConfig).port.toString();
             this.tokenTarget.textContent = (socketConfig as ISocketConfig).token;
+        }
+    }
+
+    public async startServer(e: MouseEvent) {
+        e.preventDefault();
+
+        try {
+            // await invoke('start_server');
+
+            this.startbtnTarget.disabled = true;
+            this.stopbtnTarget.disabled = false;
+            this.servivestatusTarget.innerText = 'Started';
+        } catch (err: any) {
+            this.startbtnTarget.disabled = false;
+            this.stopbtnTarget.disabled = true;
+            this.servivestatusTarget.innerText = 'Stopped';
+        }
+    }
+
+    public stopServer(e: MouseEvent) {
+        e.preventDefault();
+
+        try {
+            // await invoke('stop_server');
+
+            this.startbtnTarget.disabled = false;
+            this.stopbtnTarget.disabled = true;
+            this.servivestatusTarget.innerText = 'Stopped';
+        } catch (err: any) {
+            this.startbtnTarget.disabled = true;
+            this.stopbtnTarget.disabled = false;
+            this.servivestatusTarget.innerText = 'Started';
         }
     }
 }
